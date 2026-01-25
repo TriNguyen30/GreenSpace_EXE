957bb5e903f6cea3cbe3608cbfb4ae7edccca9e1
HenryNguyen30 <trinc0201@gmail.com>
2026-01-25 20:35:02 +0700

    Làm dropdown components để có nhu cầu dùng.

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,6 +1,6 @@
-import Navbar from "@/components/Navbar.tsx";
+import Navbar from "@/components/ui/Navbar";
 import { Outlet } from "react-router-dom";
-import Footer from "@/components/Footer.tsx";
+import Footer from "@/components/ui/Footer";
 export default function AppLayout() {
   return (
     <div>
