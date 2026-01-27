cb653959ad110ce32937dd402a5b41c2cb71f15a
HenryNguyen30 <trinc0201@gmail.com>
2026-01-27 12:48:03 +0700

    Fix typescript.json để ko cần import type

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,8 +1,4 @@
-import {
-  useDispatch,
-  useSelector,
-  type TypedUseSelectorHook,
-} from "react-redux";
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
 import type { AppDispatch, RootState } from "./store";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
