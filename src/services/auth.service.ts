1452b698d476cb2e4a815b16b1e84f85ce926fa8
HenryNguyen30 <trinc0201@gmail.com>
2026-02-08 11:34:15 +0700

    Đã chỉnh login kiểm tra phân quyền.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,44 +1,51 @@
 import { axiosInstance } from "@/lib/axios";
-import type { LoginPayload, ApiResponse, LoginResponseData, RegisterFinalizePayload, RegisterInitiatePayload, RegisterVerifyPayload, RegisterResendPayload } from "@/types/api";
+import type {
+  LoginPayload,
+  ApiResponse,
+  LoginResponseData,
+  RegisterFinalizePayload,
+  RegisterInitiatePayload,
+  RegisterVerifyPayload,
+  RegisterResendPayload,
+} from "@/types/api";
 
 export const login = async (payload: LoginPayload) => {
-    const res = await axiosInstance.post<ApiResponse<LoginResponseData>>(
-        "/Auth/login",
-        payload
-    );
+  const res = await axiosInstance.post<ApiResponse<LoginResponseData>>(
+    "/Auth/login",
+    payload,
+  );
 
-    const data = res.data.data;
+  const data = res.data.data;
 
-    return {
-        token: data.accessToken,
-        refreshToken: data.refreshToken,
-        expiresAt: data.expiresAt,
-        user: {
-            id: data.userId,
-            email: data.email,
-            name: data.fullName,
-            role: data.role,
-        },
-    };
+  return {
+    token: data.accessToken,
+    refreshToken: data.refreshToken,
+    expiresAt: data.expiresAt,
+    user: {
+      id: data.userId,
+      email: data.email,
+      name: data.fullName,
+      role: data.role,
+    },
+  };
 };
 
 export const registerInitiate = async (payload: RegisterInitiatePayload) => {
-    const res = await axiosInstance.post("/Auth/register/initiate", payload);
-    return res.data;
-}
+  const res = await axiosInstance.post("/Auth/register/initiate", payload);
+  return res.data;
+};
 
 export const registerVerify = async (payload: RegisterVerifyPayload) => {
-    const res = await axiosInstance.post("/Auth/register/verify", payload);
-    return res.data;
-}
+  const res = await axiosInstance.post("/Auth/register/verify", payload);
+  return res.data;
+};
 
 export const registerResend = async (payload: RegisterResendPayload) => {
-    const res = await axiosInstance.post("/Auth/register/resend", payload);
-    return res.data;
-}
+  const res = await axiosInstance.post("/Auth/register/resend", payload);
+  return res.data;
+};
 
 export const registerFinalize = async (payload: RegisterFinalizePayload) => {
-    const res = await axiosInstance.post("/Auth/register/finalize", payload);
-    return res.data;
-}
-
+  const res = await axiosInstance.post("/Auth/register/finalize", payload);
+  return res.data;
+};
--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -5,7 +5,7 @@ import type { LoginPayload, RegisterInitiatePayload, RegisterVerifyPayload, Regi
 export interface User {
   id: string;
   email: string;
-  name?: string;
+  fullName?: string;
   role?: string;
 }
 
